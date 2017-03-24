f68d69d3f8b18d508ed482d7fb70c5ffa1a37bd3
Pacome Beru <pacome.beru@gmail.com>
2017-03-24 16:29:47 +0100

    added licence notices in files

--- src/Semaphore.ts
+++ src/Semaphore.ts
@@ -1,3 +1,27 @@
+/*
+ * src/Semaphore.ts
+ *
+ * 
+ * Copyright (C) 2015-2017 Pierre Marchand <pierremarc07@gmail.com>
+ * Copyright (C) 2017 Pacôme Béru <pacome.beru@gmail.com>
+ *
+ *  License in LICENSE file at the root of the repository.
+ *
+ *  This file is part of waend-shell package.
+ *
+ *  waend-shell is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 3 of the License.
+ *
+ *  waend-shell is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with waend-shell.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import * as EventEmitter from 'events';
 
 
